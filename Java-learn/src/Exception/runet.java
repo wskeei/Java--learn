f2ba91527a32f471dd7ccefdb36a721ce5e02936
zeiy <1814897272@qq.com>
2023-11-29 17:51:58 +0800

    异常复习

--- Java-learn/src/Exception/runet.java
+++ Java-learn/src/Exception/runet.java
@@ -3,12 +3,12 @@ package Exception;
 public class runet {
     public void test1(){
         ArithmeticException a = new ArithmeticException("这是正常情况");
-        //ArithmeticException 为
+        //ArithmeticException 为运行时异常
         throw a;
     }
     public void test2() throws TestException {
-        TestException a = new TestException("这里需要使用throws");
-        throw a;
+        throw new TestException("这里需要使用throws");
+
     }
 
 }
